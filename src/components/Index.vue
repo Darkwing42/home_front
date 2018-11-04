9835890b12ab4b572958719d17674bf3ea764c50
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-11-04 19:54:51 +0100

    toolbar added

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,44 @@
 <template>
-  <v-container>
-    <h1>Index</h1>
-  </v-container>
+  <v-app id="app">
+
+  <v-toolbar class="primary" xs12 sm6 >
+  <v-toolbar-title>Home</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-items>
+    
+
+    
+  </v-toolbar-items>
+  </v-toolbar>
+
+    <router-view></router-view>
+    
+  <v-footer class="pa-3" id="footer">
+   <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }} J-M. Wagner</div>
+</v-footer>
+
+  
+    
+  
+
+  </v-app>
 </template>
 
 <script>
-export default {};
-</script>
 
+export default {
+  name: "App",
+  
+  data: () => ({
+      items: [
+        { title: 'Settings' , link: '/settings'},
+        { title: 'Logout', link: '/logout'},
+        
+      ]
+    })
+
+ 
+};
 <style>
 </style>
